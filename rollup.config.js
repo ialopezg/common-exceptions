e8658c0ec74837bbeb0c560a73826c5f63f90c03
Isidro A. Lopez G <me@ialopezg.com>
2020-04-16 22:05:14 -0500

    Add rollup and terser for rollup

---
+++ rollup.config.js
@@ -0,0 +1,30 @@
+import typescript from "@rollup/plugin-typescript";
+import { terser } from "rollup-plugin-terser";
+import pkg from './package.json';
+
+const filename = pkg.main.replace('.js', '');
+const outputDir = 'dist/lib';
+
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+  input: `src/${filename}.ts`,
+  output: [
+    { dir: outputDir, entryFileNames: `${filename}.min.js`, format: 'cjs', sourcemap: true },
+    { dir: outputDir, entryFileNames: `${filename}.js`, format: 'cjs', sourcemap: true },
+    { dir: outputDir, entryFileNames: `${filename}-[format].js`, format: 'es', sourcemap: true },
+    { dir: outputDir, entryFileNames: `${filename}-[format].js`, format: 'umd', sourcemap: true, name: 'CustomErrorService' },
+    { dir: outputDir, entryFileNames: `${filename}-[format].js`, format: 'iife', sourcemap: true, name: 'CustomErrorService' }
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {})
+  ],
+  plugins: [
+    typescript({
+      typescript: require('typescript')
+    }),
+    production && terser({
+      include: [/^.+\.min\.js$/, "*esm*"]
+    })
+  ]
+}
